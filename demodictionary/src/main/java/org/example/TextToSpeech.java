5db8cc54492a9206af644d92230010e73394c33c
nguyendamkien <kientrungx10st@gmail.com>
2023-11-07 17:35:01 +0700

    Create TextToSpeech.java

---
+++ demodictionary/src/main/java/org/example/TextToSpeech.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import javazoom.jl.player.Player;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class TextToSpeech {
+    /**
+     * Play sound word English.
+     */
+    public static void playSoundEnglish(String text) {
+        try {
+            String api = "https://translate.google.com/translate_tts?ie=UTF-8&tl="
+                    + "en"
+                    + "&client=tw-ob&q="
+                    + URLEncoder.encode(text, StandardCharsets.UTF_8);
+            URL url = new URL(api);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            InputStream audio = con.getInputStream();
+            new Player(audio).play();
+            con.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Error in getting voices");
+        }
+    }
+
+    /**
+     * Play sound word Vietnamese.
+     */
+    public static void playSoundVietnamese(String text) {
+        try {
+            String api = "https://translate.google.com/translate_tts?ie=UTF-8&tl="
+                    + "vi"
+                    + "&client=tw-ob&q="
+                    + URLEncoder.encode(text, StandardCharsets.UTF_8);
+            URL url = new URL(api);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            InputStream audio = con.getInputStream();
+            new Player(audio).play();
+            con.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Error in getting voices");
+        }
+    }
+}
