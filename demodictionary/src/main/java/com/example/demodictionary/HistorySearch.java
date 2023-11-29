d38261ed198ca47b615be7a9ba85fc0d740a3350
nguyendamkien <kientrungx10st@gmail.com>
2023-11-29 08:51:52 +0700

    Update HistorySearch.java

--- demodictionary/src/main/java/com/example/demodictionary/HistorySearch.java
+++ demodictionary/src/main/java/com/example/demodictionary/HistorySearch.java
@@ -1,9 +1,11 @@
 package com.example.demodictionary;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class HistorySearch {
+  private final String filePath = "E:\\OOP (BTL)\\BTL\\src\\main\\java\\org\\example\\history.txt";
   private static List<String> searchHistory;
 
   public HistorySearch() {
@@ -11,21 +13,31 @@ public class HistorySearch {
   }
 
   /**
+   * import from File.
+   */
+  public void importFromFile() {
+    try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        searchHistory.add(line);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
    * add to history.
    */
-  public void addToHistory(String searchTerm) {
-    searchHistory.add(searchTerm);
+  public void addToHistory(String search) {
+    if (searchHistory.contains(search)) {
+      searchHistory.remove(search);
+    }
+    searchHistory.add(search);
   }
 
   public List<String> getSearchHistory() {
     return searchHistory;
-  }
-
-  /**
-   * clear History.
-   */
-  public void clearHistory() {
-    searchHistory.clear();
   }
 
   /**
@@ -37,4 +49,19 @@ public class HistorySearch {
       System.out.println(searchHistory.get(i));
     }
   }
+
+  /**
+   * Export search history to file.
+   */
+  public void exportToFile() {
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+      for (String search : searchHistory) {
+        writer.write(search);
+        writer.newLine();
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
+
